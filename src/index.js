d4bb37979bf50175551438e19b16de365d126301
THIAGO SAUD <126628718+thiagosauddev@users.noreply.github.com>
2024-01-12 03:35:38 -0300

    release/1.5.2 rc (#26)
    * feat: 🎸 Created Animate Counter
    
    * feat: 🎸 Created Typewriter Animation
    
    * feat: 🎸 Updated Yarn.lock
    
    * fix: 🐛 Correction in PurgeCSS [WEBPACK]
    
    * feat: 🎸 Updated Version
    
    * feat: 🎸 Created Animated Back to Top
    
    * style: 💄 Removed Letter Spacing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-// SCRIPTS
+// SCRIPTS [IMPORTANTS]
 import "@scripts/bootstrap.min.js";
 import "@scripts/particles.js";
 import "@scripts/google-analytics.js";
+
+// SCRIPTS [ANIMATIONS]
+import "@scripts/animate-back-to-top.js";
+import "@scripts/animate-counter.js";
 
 // SCSS
 import "@scss/index.scss";
